9c8f18807de75018e8ac9fbf84af8e2d6ed597e1
Ali Özkahraman <aliozkahraman44@gmail.com>
2021-04-12 22:59:28 +0300

    Removing linear trend(detrending)

--- part1-time_series_denoising/linear_detrending.py
+++ part1-time_series_denoising/linear_detrending.py
@@ -3,17 +3,18 @@ import scipy.signal
 import matplotlib.pyplot as plt
 
 n = 2000
-signal = np.cumsum(np.random.randn(n)) + np.linspace(-30,30,n) # signal with linear trending
+signal = np.cumsum(np.random.randn(n)) + np.linspace(-30,30,n)
 
 # linear detrending
 detsignal = scipy.signal.detrend(signal)
 
-# get means
+# get meansAttributeError: module 'scipy' has no attribute 'signal'
 omean = np.mean(signal) # original mean
 dmean = np.mean(detsignal) # detrended mean
 
-# plotting
-plt.plot(range(0,n), signal, label='Original, mean=%d' %omean)
-plt.plot(range(0,n), detsignal, label='Detrended mean=%d'%dmean)
+# plot signal and detrended signal
+plt.plot(range(0,n),signal,label='Original, mean=%d' %omean)
+plt.plot(range(0,n),detsignal,label='Detrended, mean=%d' %dmean)
+
 plt.legend()
 plt.show()
